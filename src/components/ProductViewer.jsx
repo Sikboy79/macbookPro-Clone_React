d672c4de6235d566467462241b0666aeeb25d846
Sikboy79 <custombasewelds@yahoo.com>
2025-10-19 09:52:11 -0600

    zustand errors

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -12,12 +12,13 @@ const ProductViewer = () => {
                     <div className='bg-neutral-300' />
                     <div className='bg-neutral-900' />
                 </div>
-                <div className='size-control'>
+                <div className="size-control">
                     <div><p>14"</p></div>
                     <div><p>16"</p></div>
                 </div>
             </div>
         </div>
+        <p className='text-white text-4xl'>Render Canvas</p>
     </section>
   )
 }
